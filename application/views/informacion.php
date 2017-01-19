832e6f78b1fbd6684b4261c39e9f087f5f90d5f9
Andres Sandoval <sistemas04@destinoseguro.net>
2017-01-19 14:34:27 -0500

    primer comit

---
+++ application/views/informacion.php
@@ -0,0 +1,77 @@
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <h4><b>Ingresa tus datos</b></h4>
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-lg-3">
+        </div>
+        <div class="col-lg-6">
+            <input type='text' class='form-control' id="nombre" name="nombre" placeholder="Nombre Completo *">
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-lg-3">
+        </div>
+        <div class="col-lg-6">
+            <input type='text' class='form-control' id="identificacion" name="identificacion" placeholder="Numero de indetificacion *">
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-lg-3">
+        </div>
+        <div class="col-lg-6">
+            <input type='text' class='form-control' id="correo" name="correo" placeholder="Correo Electronico *">
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-lg-3">
+        </div>
+        <div class="col-lg-3">
+            <input type='text' class='form-control' id="telefono" name="telefono" placeholder="Telefono *">
+        </div>
+        <div class="col-lg-3">
+            <input type='text' class='form-control' id="ciudad" name="ciudad" placeholder="Ciudad de Residencia *">
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-lg-3">
+        </div>
+        <div class="col-lg-6">
+            <input type='text' class='form-control' id="lugar" name="lugar" placeholder="Lugar donde realizo la compra del traje *">
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-lg-3">
+        </div>
+        <div class="col-lg-6">
+            <input type='text' class='form-control' id="fecha" name="fecha" placeholder="Fecha de la compra del traje *">
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-lg-3">
+        </div>
+        <div class="col-lg-6">
+            <input type='text' class='form-control' id="factura" name="factura" placeholder="Numero de factura de la compra *">
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-lg-3">
+        </div>
+        <div class="col-lg-3">
+            <button type="button" class="btn btn-info" data-dismiss="modal">Participar</button>
+        </div>
+        <div class="col-lg-3">
+            <label><input type="radio" name="optradio"> Acepto Terminos y Condiciones</label>
+        </div>
+    </div>
+</div>
---
+++ application/views/inicio.php
@@ -0,0 +1,30 @@
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+<br>
+<div class="container-fluid">
+    <ul class="nav nav-tabs" id="tabs">
+        <li class="active trigger"><a href="#listado" data-toggle="tab">Listado</a></li>
+        <li class="trigger"><a href="#gestion" data-toggle="tab">Gestion</a></li>
+    </ul>
+    <div class="tab-content row-container">
+        <div class="tab-pane active triger" id="listado">
+            <div class="table-responsive table-responsive-lx" align="center">
+                <?php
+                    $this->load->view("informacion");
+                ?>
+            </div>
+        </div>
+        <div class="tab-pane table-responsive-lx" id="gestion">
+            <div class="container">
+                <?php
+                    $this->load->view("consulta");
+                ?>
+            </div>
+        </div>                
+    </div>
+</div>
